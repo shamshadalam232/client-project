dad6dfb2c24eef9a8d9a41def53cbe31e57d7f72
shamshad <shamshadalam10313@gmail.com>
2026-05-29 22:37:15 +0530

    kuch change kya

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = db.connections[0].readyState === 1;
 
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.log("DB ERROR:", error.message);
+    throw error;
   }
 };
 
